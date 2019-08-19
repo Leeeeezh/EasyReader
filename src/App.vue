1bf1aff3916aedc06196ba4bfe19dc84bdcd6f73
Zihua Li <lzh1757657118@outlook.com>
2019-08-19 20:26:35 +0800

    初始化项目目录和工程配置

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div class="home">
+      <div id="read">hahaha</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+  document.addEventListener('DOMContentLoaded', () => {
+    let font_size = window.innerWidth / 10
+    font_size = font_size > 50 ? 50 : font_size
+    document.querySelector('html').style.fontSize = font_size + 'px'
+  })
+
+</script lang="scss" scoped>
+@import "./assets/style/global"
+<style lang="scss" scoped>
+</style>
